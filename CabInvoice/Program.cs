6f58e6f1c0dc70a392942b35ffbaa27aa1caf7b9
Mathura <mathuradas333@gmail.com>
2020-11-03 23:29:10 +0530

    [Mathura]Add. Added Welcome message

---
+++ CabInvoice/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CabInvoice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cab Invoice Generator");
+            Console.WriteLine("================================");
+        }
+    }
+}
